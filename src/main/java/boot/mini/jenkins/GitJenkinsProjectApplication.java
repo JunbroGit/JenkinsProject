87274e7b038084e5dd66760af02958e935f48fb0
junbbangz <goo2913@naver.com>
2024-06-10 12:36:25 +0900

    0610

--- src/main/java/boot/mini/jenkins/GitJenkinsProjectApplication.java
+++ src/main/java/boot/mini/jenkins/GitJenkinsProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("{data.*}")
+@ComponentScan({"data.*"})
 public class GitJenkinsProjectApplication {
 
 	public static void main(String[] args) {
